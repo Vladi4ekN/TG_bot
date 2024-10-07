49579affb529106487d68b857ccac45a520ce2e5
Vladi4ekN <167472798+Vladi4ekN@users.noreply.github.com>
2024-10-08 00:15:38 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+
+
+@dp.message_handler(text = ['Hello', 'Hi'])
+async def Vlad_massage(message):
+    print("Vlad message")
+
+@dp.message_handler(commands = ['start'])
+async def start_massage(massage):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_massage(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
